2131a5489f3c09d83b7f94d203f7fa80eb315a1f
kahverengi <ahmet@duruer.dev>
2025-01-07 18:45:25 +0300

    fix: force plugin config migration parameters for better API support
    BREAKING CHANGE:
    * PluginConfigMigration variables moved to constructor method (from, to, versionInfo).

--- src/main/kotlin/co/statu/parsek/api/config/PluginConfigMigration.kt
+++ src/main/kotlin/co/statu/parsek/api/config/PluginConfigMigration.kt
@@ -2,11 +2,7 @@ package co.statu.parsek.api.config
 
 import io.vertx.core.json.JsonObject
 
-abstract class PluginConfigMigration {
-    abstract val from: Int
-    abstract val to: Int
-    abstract val versionInfo: String
-
+abstract class PluginConfigMigration(val from: Int, val to: Int, val versionInfo: String) {
     fun isMigratable(version: Int) = version == from
 
     abstract fun migrate(config: JsonObject)
